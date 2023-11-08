5c4f920b1b34be16cff8cfbbdb9bee49414cb763
razibraj <razibraj56@gmail.com>
2023-11-09 01:58:16 +0600

    responsive the room section

--- src/pages/Home/AvailableRooms/AvailAbleRooms.jsx
+++ src/pages/Home/AvailableRooms/AvailAbleRooms.jsx
@@ -2,7 +2,6 @@ import { useLoaderData } from "react-router-dom";
 import Footer from "../../Shared/Footer/Footer";
 import Navbar from "../../Shared/Navbar/Navbar";
 import Available from "./Available";
-import ProductFilter from "./ProductFilter";
 import { useEffect, useState } from "react";
 
 const AvailAbleRooms = () => {
@@ -22,58 +21,59 @@ const AvailAbleRooms = () => {
     // }
 
     useEffect(() => {
-        if(selectedValue === 'low'){
+        if (selectedValue === 'low') {
             fetch('http://localhost:5000/sortedPrice/low')
 
-            .then(res => res.json())
-            .then(data => setAvailables(data))
+                .then(res => res.json())
+                .then(data => setAvailables(data))
         }
-        else if(selectedValue === 'high'){
+        else if (selectedValue === 'high') {
             fetch('http://localhost:5000/sortedPrice/high')
 
-            .then(res => res.json())
-            .then(data => setAvailables(data))
+                .then(res => res.json())
+                .then(data => setAvailables(data))
         }
-        else{
+        else {
             fetch('http://localhost:5000/availableRooms')
 
-            .then(res => res.json())
-            .then(data => setAvailables(data))
+                .then(res => res.json())
+                .then(data => setAvailables(data))
         }
 
-        
+
     }, [selectedValue])
 
     const handleOnChange = (event) => {
         const selectedOptionValue = event.target.value;
         setSelectedValue(selectedOptionValue);
         // console.log(selectedOptionValue);
-      };
+    };
 
-    
+
 
     return (
         <div>
             <div className="backdrop-brightness-50">
                 <Navbar></Navbar>
             </div>
-            <div className="text-center mt-10">
-                <ProductFilter></ProductFilter>
+            <div className="lg:ml-56 mt-12">
+                <form>
+                    <select onChange={handleOnChange} className="select select-bordered w-full max-w-xs" name="sortPrice" value={selectedValue}>
+                        <option value={'all'}>All Price</option>
+                        <option value={'high'}>Price high</option>
+                        <option value={'low'}>Price low</option>
+                    </select>
+                </form>
             </div>
-            <form>
-                <select onChange={handleOnChange} className="select select-bordered w-full max-w-xs" name="sortPrice" value={selectedValue}>
-                    <option value={'all'}>All Price</option>
-                    <option value={'high'}>Price high</option>
-                    <option value={'low'}>Price low</option>
-                </select>
-            </form>
             <div className="grid grid-cols-1 lg:mx-56 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
                 {
                     availables?.map(available => <Available key={available._id} available={available}></Available>)
                 }
             </div>
 
-            <Footer></Footer>
+            <div className="mt-12">
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
