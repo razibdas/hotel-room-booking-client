51504b8816813586f7b0ebc59092644fd5a9ce0e
razibraj <razibraj56@gmail.com>
2023-11-09 15:16:44 +0600

    show the booking

--- src/pages/Shared/Mybooking/Mybooking.jsx
+++ src/pages/Shared/Mybooking/Mybooking.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import  { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../../providers/AuthProvider';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 const Mybooking = () => {
+
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([])
+
+    const url = `http://localhost:5000/bookingsAll?email=${user?.email}`
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBookings(data))
+    }, [])
+
     return (
         <div>
-            this is booking
+            <div className='backdrop-brightness-50'>
+                <Navbar></Navbar>
+            </div>
+            <div>
+                <h2>My Boookings: {bookings.length}</h2>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
