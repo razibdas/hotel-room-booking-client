d92593fedf67bb9be04811c7ec72aa0336890ea8
razibraj <razibraj56@gmail.com>
2023-11-09 11:48:11 +0600

    addedt the location in hotel room

--- src/pages/Home/Details/Details.jsx
+++ src/pages/Home/Details/Details.jsx
@@ -16,41 +16,53 @@ const Details = () => {
     const details = useLoaderData();
 
     console.log(details);
-    const { price, description, image } = details;
+    const { price, description, image, title, size , place} = details;
 
     return (
         <div>
             <div className="backdrop-brightness-50">
                 <Navbar ></Navbar>
             </div>
-            <div className='w-[600px] h-[600px] mx-auto'>
-                <Swiper
-                    effect={'coverflow'}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={'auto'}
-                    coverflowEffect={{
-                        rotate: 50,
-                        stretch: 0,
-                        depth: 100,
-                        modifier: 1,
-                        slideShadows: true,
-                    }}
-                    pagination={true}
-                    modules={[EffectCoverflow, Pagination]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide >
-                        <img className='' src={image} />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src="https://i.ibb.co/fx4FS1h/sara-dubler-Koei-7y-Yt-Io-unsplash-1.jpg" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src="https://i.ibb.co/Y8xq864/visualsofdana-T5p-L6ci-En-I-unsplash.jpg" />
-                    </SwiperSlide>
+            <div className="lg:flex mt-12 gap-8 justify-center">
+                <div className='w-[600px] h-[600px]'>
+                    <Swiper
+                        effect={'coverflow'}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={'auto'}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        pagination={true}
+                        modules={[EffectCoverflow, Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide >
+                            <img className='' src={image} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://i.ibb.co/fx4FS1h/sara-dubler-Koei-7y-Yt-Io-unsplash-1.jpg" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://i.ibb.co/Y8xq864/visualsofdana-T5p-L6ci-En-I-unsplash.jpg" />
+                        </SwiperSlide>
 
-                </Swiper>
+                    </Swiper>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold">{title}</h2>
+                    <h2>Price PerNight: ${price}</h2>
+                    <h2>Size: {size} ft</h2>
+                    <h2>Available Seat:</h2>
+                    <h2>Status:</h2>
+                    <h2>Location: {place}</h2>
+                    <h2>Ratings:</h2>
+                    <h2>Description: {description}</h2>
+                </div>
             </div>
             <Footer></Footer>
         </div>
